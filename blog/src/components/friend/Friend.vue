7d1fd8ec5cc2b987a588fdf0e6fdcb57194143f9
zgh <2627191377@qq.com>
2019-11-27 16:52:50 +0800

    修补友情链接bug

--- blog/src/components/friend/Friend.vue
+++ blog/src/components/friend/Friend.vue
@@ -2,13 +2,13 @@
     <div   class="blog-home">
         <div class="blog-child">
 
-         <div v-for="(item,index) in friendList" :key="index" class="friend-item">
+         <div v-for="(item,index) in friendList" :key="index" class="friend-item" @click="See(item.link)">
             <el-row type="flex" class="row-bg" justify="space-between">
                 <el-col :span="4"><div class="grid-content bg-purple">{{item.name}}</div></el-col>
-                 <el-col :span="15"><div  class="link"><a href="https://www.baidu.com/" target="_blank">{{item.link}}</a></div></el-col>
+                 <el-col :span="15"><div  class="link">{{item.link}}</div></el-col>
             </el-row>
             <el-row type="flex" class="introduce" v-if="item.introduce!=''?true:false">
-                <el-col :span="6"><div class="grid-content bg-purple">博客介绍：{{item.introduce}}</div></el-col>         
+                <el-col :span="12"><div class="grid-content bg-purple" >博客介绍：{{item.introduce}}</div></el-col>         
             </el-row>
          </div>
         </div>
@@ -50,7 +50,11 @@ export default {
 
             }
 
+        },
+        See (e) {
+           window.open(e);
         }
+
     },
 }
 </script>
@@ -60,6 +64,8 @@ export default {
    .introduce{
        color:#9c9797;
        margin-top: 10px;
+       text-align:justify;
+       text-justify:distribute-all-lines;
    }
    .friend-item{
        box-shadow: 2px 2px 15px #eee;
